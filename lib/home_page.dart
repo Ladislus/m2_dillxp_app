d3127f35dfebfc552a491f1d10977325b8f9f833
Tom Ribardiere <tom.ribardiere@free.fr>
2022-02-10 18:23:09 +0100

    [ADD] Carousel Film fait mais code très moche

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:test_dill/drawer.dart';
 
 import 'constants.dart';
 
@@ -7,59 +8,7 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: rouge_principal,
-              ),
-              child: Text('Navigation'),
-            ),
-            ListTile(
-              title: const Text('Programmation'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Mes Billets'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Portefeuille'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            Container(
-              height: 5,
-              width: 1,
-              color: rouge_principal,
-            ),
-            ListTile(
-              title: const Text('Profil'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Deconnexion'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: CustomDrawer(),
       appBar: AppBar(
         actions: [
           Container(
@@ -73,62 +22,93 @@ class HomePage extends StatelessWidget{
         ],
         backgroundColor: rouge_appbar,
       ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(25,45,0,0),
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(25,45,0,0),
+            child: Column(
               children: [
-                Container(
-                  child: Text("à voir Aujourd'hui".toUpperCase(),
-                  style: TextStyle(fontWeight:  FontWeight.bold)),
+                Row(
+                  children: [
+                    Container(
+                      child: Text("à voir Aujourd'hui".toUpperCase(),
+                      style: TextStyle(fontWeight:  FontWeight.bold)),
+                    ),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                  children: [
+                    Container(
+                      height: 3,
+                      width: 65,
+                      color: rouge_principal,
+                    ),
+                  ],
                 ),
               ],
             ),
-            Padding(padding: EdgeInsets.only(top: 10)),
-            Row(
+          ),
+          SizedBox(height: 12),
+          SizedBox(
+            height: 400,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
               children: [
-                Container(
-                  height: 3,
-                  width: 65,
-                  color: rouge_principal,
+                Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'images/affiche_belle.jpg',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 12),
+                Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'images/violet_film.jpg',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 12),
+                Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'images/josee_film.jpg',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 12),
+                Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'images/maquia_film.jpg',
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            SizedBox(
-              width: 350,
-              height: 350,
-              child: ListView(
-                // This next line does the trick.
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Container(
-                    width: 160.0,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 160.0,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 160.0,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 160.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 160.0,
-                    color: Colors.orange,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+
+        ],
       ),
+
     );
   }
+
+
+  Widget buildCard() => Container(
+    width: 200,
+    height: 200,
+    color: Colors.red,
+  );
 
 }
