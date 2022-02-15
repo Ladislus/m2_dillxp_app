9e0744c4e9852ce00df78b19314ff3271916303a
Tom Ribardiere <tom.ribardiere@free.fr>
2022-02-15 18:52:18 +0100

    [MOD] image de fond de la landingPage + refactorisation du code de la landingPage (padding et image de fond)

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,180 +1,186 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/svg.dart' as svg_test;
 
 import 'constants.dart';
 
 class LandingPage extends StatelessWidget{
+
   @override
   Widget build( context) {
-    return  Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/test.png"), fit: BoxFit.cover)
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: ListView(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 90,
-              //color: Colors.greenAccent,
-              //child: Center(
-              //child: Text('150'),
-              //),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'images/logo-cinemas-studio.svg',
-                  height: 135,
-                  width: 135,
-                  fit: BoxFit.cover,
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(top: 25)),
-            Row(
-              children: [
-                const Padding(padding: EdgeInsets.only(right: 50)),
-                Transform.rotate(
-                  angle: 11*pi/6,
-                  child: Image.asset(
-                    'images/image_2.png',
-                    height: 80,
-                    width: 80,
-                    fit: BoxFit.cover,
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          svg_test.SvgPicture.asset(
+            'images/fond_landingPage.svg',
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+          ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 90),
+                    child: svg_test.SvgPicture.asset(
+                      'images/logo-cinemas-studio.svg',
+                      height: 135,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Transform.rotate(
-                  angle: pi/6,
-                  child: SvgPicture.asset(
-                    'images/movie-cinema-svgrepo-com.svg',
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 50,top: 25),
+                    child: Transform.rotate(
+                      angle: 11*pi/6,
+                      child: Image.asset(
+                        'images/image_2.png',
+                        height: 80,
+                        width: 80,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(right: 50),
+                    child: Transform.rotate(
+                      angle: pi/6,
+                      child: svg_test.SvgPicture.asset(
+                        'images/movie-cinema-svgrepo-com.svg',
+                        height: 50,
+                        width: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-                const Padding(padding: EdgeInsets.only(left: 50)),
-              ],
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 60)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    " Bienvenue dans l'Application des cinémas Studio !".toUpperCase(),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 25,top: 60),
+                    child: Text(
+                        " Bienvenue dans l'Application des cinémas Studio !".toUpperCase(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Réservez vos places sur mobile".toUpperCase(),
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    )
-                ),
-              ],
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 25)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Réservez vos places sur mobile".toUpperCase(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    height: 100,
-                    //color: Colors.greenAccent,
-                    //child: Center(
-                    //child: Text('100'),
-                    //),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              height: 5,
-              width: double.infinity,
-              color: rouge_principal,
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context,
-                        "/login_form");
-                  },
-                  child: Text(
-                    'Connexion'.toUpperCase(),
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
+                        fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
                     ),
                   ),
-                  style: OutlinedButton.styleFrom(
-                    elevation: 5,
-                    padding: EdgeInsets.fromLTRB(40,20,40,20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+                ],
+              ),
+              Container(
+                height: 5,
+                width: double.infinity,
+                color: rouge_principal,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 15),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context,
+                            "/login_form");
+                      },
+                      child: Text(
+                        'Connexion'.toUpperCase(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        elevation: 5,
+                        padding: EdgeInsets.fromLTRB(40,20,40,20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        backgroundColor: Colors.white,
+                        side: BorderSide(width: 2.5, color: rouge_principal),
+                      ),
                     ),
-                    backgroundColor: Colors.white,
-                    side: BorderSide(width: 2.5, color: rouge_principal),
                   ),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                      Navigator.pushNamed(context,
-                          "/register");
-                    },
-                  child: Text(
-                    'Inscription'.toUpperCase(),
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
+                  Container(
+                    padding: EdgeInsets.only(top: 15),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context,
+                            "/register");
+                      },
+                      child: Text(
+                        'Inscription'.toUpperCase(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        elevation: 5,
+                        padding: EdgeInsets.fromLTRB(40,20,40,20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        backgroundColor: Colors.white,
+                        side: BorderSide(width: 2.5, color: rouge_principal),
+                      ),
                     ),
                   ),
-                  style: OutlinedButton.styleFrom(
-                    elevation: 5,
-                    padding: EdgeInsets.fromLTRB(40,20,40,20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 15,right: 20),
+                    child: Text(
+                      "Continuer sans Connexion".toUpperCase(),
+                      style: TextStyle(
+                          color: rouge_principal,
+                          fontWeight: FontWeight.w900
+                      ),
                     ),
-                    backgroundColor: Colors.white,
-                    side: BorderSide(width: 2.5, color: rouge_principal),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  "Continuer sans Connexion".toUpperCase(),
-                  style: TextStyle(
-                      color: rouge_principal,
-                      fontWeight: FontWeight.w900
-                  ),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(20, 40, 0, 0))
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
